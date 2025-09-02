347f024bee0f17edd839af0e2f9ec6c4182b69a8
Matthew Yee <myee@redhat.com>
2025-09-02 12:39:26 -0700

    Refactor user account setup script to clone repositories into a temporary directory and update file movement commands for improved organization and clarity.

--- runtime-automation/02-useraccounts-script/setup-rhel.sh
+++ runtime-automation/02-useraccounts-script/setup-rhel.sh
@@ -3,14 +3,14 @@ echo "Starting module called 02-useraccounts-script" >> /tmp/progress.log
 
 cd /root
 
-git clone https://gist.github.com/1c8afb3087be6e4ec8f1df0a5e50124d.git /root/test
-mv /root/users/users.csv /root/
-rm -rf /root/users
+git clone https://gist.github.com/1c8afb3087be6e4ec8f1df0a5e50124d.git /root/temp
+mv /root/temp/users.csv /root/
+rm -rf /root/temp
 
 dnf install -y tcsh
 touch /root/create_user.sh
 chmod +x /root/create_user.sh
 
-git clone https://gist.github.com/88a1cebc43f6bf38301876019569bff5.git /root/test
-mv /root/test/a_modest_proposal.txt /root/
-rm -rf mv /root/test
+git clone https://gist.github.com/88a1cebc43f6bf38301876019569bff5.git /root/temp
+mv /root/temp/a_modest_proposal.txt /root/
+rm -rf mv /root/temp
