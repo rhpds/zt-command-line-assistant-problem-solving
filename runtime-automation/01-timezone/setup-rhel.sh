bdfca3cd107e3145ff3f63f629bd7dd901ea9888
Matthew Yee <myee@redhat.com>
2025-09-02 09:26:15 -0700

    Clean up subscription manager by adding 'subscription-manager clean' command in RHEL setup script.

--- runtime-automation/01-timezone/setup-rhel.sh
+++ runtime-automation/01-timezone/setup-rhel.sh
@@ -2,4 +2,5 @@
 echo "Starting module called 01-timezone" >> /tmp/progress.log
 
 rpm -e katello-ca-consumer-demosat-capsule-00.infra.demo.redhat.com-1.0-2.noarch
+subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
