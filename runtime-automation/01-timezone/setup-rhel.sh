7e7eaf78cede6d1f72b8da970738432403c0a21e
Matthew Yee <myee@redhat.com>
2025-09-02 10:14:46 -0700

    Add command-line-assistant installation to RHEL setup script for enhanced functionality.

--- runtime-automation/01-timezone/setup-rhel.sh
+++ runtime-automation/01-timezone/setup-rhel.sh
@@ -6,3 +6,4 @@ KATELLO=$(rpm -qa | grep katello-ca)
 rpm -e $KATELLO
 subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
+dnf install -y command-line-assistant
