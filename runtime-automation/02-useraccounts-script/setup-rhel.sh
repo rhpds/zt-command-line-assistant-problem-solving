c2598c6eaea7b2248f733a17040323faa3d03a81
Matthew Yee <myee@redhat.com>
2025-09-02 10:21:42 -0700

    Update timezone configuration command for RHEL 10 and enhance user account setup script with additional functionalities.

--- runtime-automation/02-useraccounts-script/setup-rhel.sh
+++ runtime-automation/02-useraccounts-script/setup-rhel.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 echo "Starting module called 02-useraccounts-script" >> /tmp/progress.log
+
+git clone https://gist.github.com/1c8afb3087be6e4ec8f1df0a5e50124d.git
+mv 1c8afb3087be6e4ec8f1df0a5e50124d/users.csv .
+rm -rf 1c8afb3087be6e4ec8f1df0a5e50124d
+
+dnf install -y tcsh
+touch /root/create_user.sh
+chmod +x /root/create_user.sh
+
+git clone https://gist.github.com/88a1cebc43f6bf38301876019569bff5.git
+mv 88a1cebc43f6bf38301876019569bff5/a_modest_proposal.txt .
+rm -rf mv 88a1cebc43f6bf38301876019569bff5
--- runtime-automation/03-web-server/setup-rhel.sh
+++ runtime-automation/03-web-server/setup-rhel.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 echo "Starting module called 03-web-server" >> /tmp/progress.log
+
+systemctl enable --now httpd.service
