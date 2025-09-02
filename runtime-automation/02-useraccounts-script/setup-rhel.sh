491b7df9f4a58b0aea72de9b7b44f0b466d876fa
Matthew Yee <myee@redhat.com>
2025-09-02 11:08:12 -0700

    Refactor user account setup script to clone repositories into a dedicated /root/test directory and update file movement commands for improved organization and clarity.

--- runtime-automation/02-useraccounts-script/setup-rhel.sh
+++ runtime-automation/02-useraccounts-script/setup-rhel.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Starting module called 02-useraccounts-script" >> /tmp/progress.log
 
-git clone https://gist.github.com/1c8afb3087be6e4ec8f1df0a5e50124d.git /root/
-mv /root/1c8afb3087be6e4ec8f1df0a5e50124d/users.csv /root/
-rm -rf /root/1c8afb3087be6e4ec8f1df0a5e50124d
+git clone https://gist.github.com/1c8afb3087be6e4ec8f1df0a5e50124d.git /root/test
+mv /root/users/users.csv /root/
+rm -rf /root/users
 
 dnf install -y tcsh
 touch /root/create_user.sh
 chmod +x /root/create_user.sh
 
-git clone https://gist.github.com/88a1cebc43f6bf38301876019569bff5.git /root/
-mv /root/88a1cebc43f6bf38301876019569bff5/a_modest_proposal.txt /root/
-rm -rf mv /root/88a1cebc43f6bf38301876019569bff5
+git clone https://gist.github.com/88a1cebc43f6bf38301876019569bff5.git /root/test
+mv /root/test/a_modest_proposal.txt /root/
+rm -rf mv /root/test
