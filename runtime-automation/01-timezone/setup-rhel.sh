379301addfee08f7e361cc5c013249787558db78
Matthew Yee <myee@redhat.com>
2025-09-02 09:44:28 -0700

    Refactor RHEL setup script to dynamically remove Katello CA consumer package before subscription registration.

--- runtime-automation/01-timezone/setup-rhel.sh
+++ runtime-automation/01-timezone/setup-rhel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Starting module called 01-timezone" >> /tmp/progress.log
 
-rpm -e katello-ca-consumer-demosat-capsule-00.infra.demo.redhat.com-1.0-2.noarch
+KATELLO=$(rpm -qa | grep katello-ca)
+
+rpm -e $KATELLO
 subscription-manager clean
 subscription-manager register --activationkey=12-5-22-instruqt --org=12451665 --force
